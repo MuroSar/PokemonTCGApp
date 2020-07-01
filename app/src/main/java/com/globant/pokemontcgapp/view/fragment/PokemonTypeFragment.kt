b3cf66cef24e6adaf6fbe07fcf53bcad168e1cc1
Marcos Cifuentes <marcos.cifuentes@globant.com>
2020-07-01 13:10:19 -0300

    added fragments layouts and classes

---
+++ app/src/main/java/com/globant/pokemontcgapp/view/fragment/PokemonTypeFragment.kt
@@ -0,0 +1,20 @@
+package com.globant.pokemontcgapp.view.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.globant.pokemontcgapp.R
+
+class PokemonTypeFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
+        inflater.inflate(R.layout.fragment_pokemon_type_layout, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    // TODO ViewModel implementation and functionalities
+}
