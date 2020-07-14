a32c27e4bc41ea283b8868a4abc172fadda6b8fd
Marcos Cifuentes <marcos.cifuentes@globant.com>
2020-07-14 11:54:52 -0300

    initialize stetho

--- app/src/main/java/com/globant/pokemontcgapp/SampleApplication.kt
+++ app/src/main/java/com/globant/pokemontcgapp/SampleApplication.kt
@@ -1,6 +1,9 @@
 package com.globant.pokemontcgapp
 
 import android.app.Application
+import com.facebook.stetho.Stetho
+import com.globant.di.databaseModule
+import com.globant.di.repositoryModule
 import com.globant.di.serviceModule
 import com.globant.di.useCaseModule
 import com.globant.pokemontcgapp.di.viewModelModule
@@ -10,10 +13,11 @@ import org.koin.core.context.startKoin
 class SampleApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+        Stetho.initializeWithDefaults(this)
 
         startKoin {
             androidContext(this@SampleApplication)
-            modules(listOf(viewModelModule, useCaseModule, serviceModule))
+            modules(listOf(viewModelModule, useCaseModule, serviceModule, databaseModule, repositoryModule))
         }
     }
 }
