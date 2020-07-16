6932c720f2a692183aba2e3e10d940401693d147
Marcos Cifuentes <marcos.cifuentes@globant.com>
2020-07-16 10:03:53 -0300

    fixing comments

--- data/src/main/java/com/globant/data/mapper/BaseMapper.kt
+++ data/src/main/java/com/globant/data/mapper/BaseMapper.kt
@@ -1,8 +1,10 @@
 package com.globant.data.mapper
 
+/**
+ * This BaseMapper needed a third generic value to handle the different hash maps that
+ * deliver the resources for the entities, and it's nullable and initialized null to avoid
+ * errors on parameters insertions
+ */
 interface BaseMapper<E, D, R> {
     fun transform(type: E, resources: R? = null): D
-    // This BaseMapper needed a third generic value to handle the different hash maps that
-    // deliver the resources for the entities, and it's nullable and initialized null to avoid
-    // errors on parameters insertions
 }
